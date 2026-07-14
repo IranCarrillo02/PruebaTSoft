6fa69bc3235c4cb7440f59fbf7dcb76e26961bf5
IranCarrillo <iran_carrillo_g@outlook.com>
2026-07-14 15:25:59 -0600

    chore: add initial SwiftUI project scaffold
    Default Xcode template (iOS 26, Swift 5) for the Pokedex technical
    assessment, with a flattened standard project layout and .DS_Store
    excluded via .gitignore. Clean Architecture layers (Domain/Data/
    Presentation) will be added on top of this in following commits.

---
+++ PruebaTSoft/PruebaTSoft/PruebaTSoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaTSoftApp.swift
+//  PruebaTSoft
+//
+//  Created by IRAN CARRILLO GUZMAN on 14/07/26.
+//
+
+import SwiftUI
+
+@main
+struct PruebaTSoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PruebaTSoft/PruebaTSoftTests/PruebaTSoftTests.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaTSoftTests.swift
+//  PruebaTSoftTests
+//
+//  Created by IRAN CARRILLO GUZMAN on 14/07/26.
+//
+
+import Testing
+@testable import PruebaTSoft
+
+struct PruebaTSoftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
